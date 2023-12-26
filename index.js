a0ec2122b58fa8be6588ecd7bd8685b03da8898f
José Cipriano <joseiraildesciprianoribeiro@gmail.com>
2023-12-26 20:57:28 -0300

    ConnectU(/index.js): Error in line 18

--- index.js
+++ index.js
@@ -15,8 +15,8 @@ app.use(bodyParser.json())
 app.use('css/', express.static('css/'))
 
 app.get('/', (req, res)=>{
-    connection.query("SELECT * FROM posts", (results, fields)=>{
-        res.render('home', { post: fields })
+    Posts.findAll().then(function(post){
+        res.render('home', { post: post })
     })
 })
 
