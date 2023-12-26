af6f745ff61c20509ca54fa563095b5e07d49781
José Cipriano <joseiraildesciprianoribeiro@gmail.com>
2023-12-26 12:26:22 -0300

    ConnectU(index.js): Importing the class Posts in the index.js

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ import express from "express"
 import { engine } from "express-handlebars"
 import bodyParser from "body-parser"
 import { Sequelize } from "sequelize"
+import sequelize from "./db/db"
+import Posts from "./post/post"
 import { createConnection } from "mysql2"
 import path from 'path'
 const app = express()
