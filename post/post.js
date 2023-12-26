299bf075cd82538ff26c07ee6592947eb3d1c363
José Cipriano <joseiraildesciprianoribeiro@gmail.com>
2023-12-26 11:54:24 -0300

    ConnectU(post.js): Update post.js

--- post/post.js
+++ post/post.js
@@ -1,4 +1,9 @@
 import { Sequelize } from "sequelize";
 import dotenv from 'dotenv/config.js'
 
-const Post = new Sequelize()
+const sequelize = new Sequelize(process.env.DATABASE_NAME, process.env.DATABASE_USER, '', {
+    dialect: 'mysql',
+    host: process.env.DATABASE_HOST
+})
+
+export default sequelize
