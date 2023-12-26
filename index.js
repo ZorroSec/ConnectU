a5dfecaf863de63616526593fbf3bab90e9a8066
José Cipriano <joseiraildesciprianoribeiro@gmail.com>
2023-12-26 10:30:23 -0300

    ConnectU(index.js): Upload index.js

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 import express from "express"
 import { engine } from "express-handlebars"
 import bodyParser from "body-parser"
+import { Sequelize } from "sequelize"
+import { createConnection } from "mysql2"
 import path from 'path'
 const app = express()
 app.engine('handlebars', engine())
@@ -9,3 +11,4 @@ app.set('views', './views')
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 app.use(express.static(path.join(__dirname + 'public')))
+
