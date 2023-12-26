98eda8e88c665a6c674e92713555f61f45dafda0
José Cipriano <joseiraildesciprianoribeiro@gmail.com>
2023-12-26 10:25:57 -0300

    ConnectU(index.js): Upload index.js

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
-import express from "express";
-import { engine } from "express-handlebars";
-import bodyParser from "body-parser";
+import express from "express"
+import { engine } from "express-handlebars"
+import bodyParser from "body-parser"
+import path from 'path'
 const app = express()
 app.engine('handlebars', engine())
 app.set('view engine', 'handlebars')
 app.set('views', './views')
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
+app.use(express.static(path.join(__dirname + 'public')))
